5cb05f72233733702268e591e9d5c568f1c9d47b
Carol Ko <carol.ko1823@gmail.com>
2024-11-10 14:54:27 -0800

    formated ListOfParking

--- src/ListOfParking.js
+++ src/ListOfParking.js
@@ -3,21 +3,22 @@ import "./carol.css";
 
 function ListOfParkingDisplay() {
   return (
-    <div>
+    <div className = "layout">
       <h1 className="title">Here is a list of all the parking lots at UBC.</h1>
-      <h1>Please select the one you would like to rate</h1>
+      <h1 className="directions">Please select the one you would like to rate: </h1>
+        
         <div id="welcomeImg" className="parking">
         
             <div className ="image-text">
                 <img src="./orchardCommons.png" alt="Place Vanier" className = "parkingLotImage"/>
-                        <div className="locationCaption">
-                            <h4>Orchard Commons</h4>
-                            <p>Crowdness: </p>
-                        </div>
+                    <div className="locationCaption">
+                        <h4>Orchard Commons</h4>
+                        <p>Crowdness: </p>
+                    </div>
             </div>
 
             <div className ="image-text">
-            <img src="./placeVanier.png" alt="Place Vanier" className = "parkingLotImage"/>
+                <img src="./placeVanier.png" alt="Place Vanier" className = "parkingLotImage"/>
                     <div className="locationCaption">
                         <h4>Place Vanier</h4>
                         <p>Crowdness: </p>
@@ -25,16 +26,16 @@ function ListOfParkingDisplay() {
             </div>
 
             <div className ="image-text">
-            <img src="./totemPark.png" alt="Totem Park" className = "parkingLotImage"/>
+                <img src="./totemPark.png" alt="Totem Park" className = "parkingLotImage"/>
                     <div className="locationCaption">
-                    <h4>Totem Park</h4>
-                    <p>Crowdness: </p>
+                        <h4>Totem Park</h4>
+                        <p>Crowdness: </p>
                     </div>
             </div>
         
         
             <div className ="image-text">
-            <img src="./walterGage.png" alt="Totem Park" className = "parkingLotImage"/>
+                <img src="./walterGage.png" alt="Totem Park" className = "parkingLotImage"/>
                     <div className="locationCaption">
                         <h4>Walter Gage</h4>
                         <p>Crowdness: </p>
