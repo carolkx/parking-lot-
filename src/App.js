8b23dece65138f8c2fbd3630d79fcedbe52270f7
ftjcai10 <ftjcai10@students.cs.ubc.ca>
2024-11-10 12:49:36 -0800

    add change to index so we can have subpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import './thumb.css';
+import thumbUpImage from './thumbsUp.png';  
+import thumbDownImage from './thumbsDown.png';
 
 function App() {
-    return (
-      <div>
-          <h1>Hello!</h1>
-      </div>
-    );
-  }
   
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="buttons-container">
+          <img
+            src={thumbUpImage}
+            alt="Thumb Up"
+            className="thumb-button"
+          />
+          <img
+            src={thumbDownImage}
+            alt="Thumb Down"
+            className="thumb-button"
+          />
+        </div>
+      </header>
+    </div>
+  );
+}
+
 export default App;
