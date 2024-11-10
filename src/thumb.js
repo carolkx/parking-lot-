2b36d733592c3f10a0e6cecc1e03cf788b961019
ftjcai10 <ftjcai10@students.cs.ubc.ca>
2024-11-10 14:20:56 -0800

    thumb update

--- src/thumb.js
+++ src/thumb.js
@@ -1,27 +1,16 @@
-import './thumb.css';
-import thumbUpImage from './thumbsUp.png'; 
-import thumbDownImage from './thumbsDown.png';
+import "./thumb.css";
+import thumbUpImage from "./thumbsUp.png";
+import thumbDownImage from "./thumbsDown.png";
 
 function Thumb() {
-  
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-      <h2>Give A Rating! </h2> 
-      <h1> Was the parking lot decently empty? </h1>
-      </div>
+          <h2>Give A Rating! </h2>
+          <h1> Was the parking lot decently empty? </h1>
         <div className="thumb-button">
-          <img
-            src={thumbUpImage}
-            alt="Thumb Up"
-            className="thumb-button"
-          />
-          <img
-            src={thumbDownImage}
-            alt="Thumb Down"
-            className="thumb-button"
-          />
+          <img src={thumbUpImage} alt="Thumb Up" className="thumb-button" />
+          <img src={thumbDownImage} alt="Thumb Down" className="thumb-button" />
         </div>
       </header>
     </div>
