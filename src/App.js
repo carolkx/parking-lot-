84948f938cab36212f75a8a352d2118c00209c34
njerem01 <njerem01@students.cs.ubc.ca>
2024-11-10 11:06:10 -0800

    hhsfgvgsg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div>
-        <h1>WELCOME TO</h1>;
-        <h1>PARKING LOT</h1>;
-        
+      <h1>WELCOME TO</h1>;<h1>PARKING LOT</h1>;
+      <div>
+        <img src="./car.img.png" alt="car photo" />
+      </div>
     </div>
   );
 }
