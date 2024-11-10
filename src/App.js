ff562c00975fab7af16c582986e910ab4572b0b7
Carol Ko <carol.ko1823@gmail.com>
2024-11-10 13:51:07 -0800

    carol commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ListOfParkingDisplay from "./ListOfParking";
 
 function App() {
   return (
-    <div class="master">
-      <h1>WELCOME TO</h1>
-      <h1>PARKING LOT</h1>
-      <div class="parent">
-        <img src="./parking.img.png" alt="parking photo" class="image1" />
-        <img src="./car.img.png" alt="car photo" class="image2" />
-      </div>
-      <img src="./tBirdLeft.img.png" alt="bird photo" class="birdLeft" />
+    // <div class="master">
+    //   <h1>WELCOME TO</h1>
+    //   <h1>PARKING LOT</h1>
+    //   <div class="parent">
+    //     <img src="./parking.img.png" alt="parking photo" class="image1" />
+    //     <img src="./car.img.png" alt="car photo" class="image2" />
+    //   </div>
+    //   <img src="./tBirdLeft.img.png" alt="bird photo" class="birdLeft" />
 
-      <button class="button">GET STARTED</button>
+    //   <button class="button">GET STARTED</button>
 
-      <img src="./tBirdR.img.png" alt="bird photo" class="birdRight" />
-    </div>
+    //   <img src="./tBirdR.img.png" alt="bird photo" class="birdRight" />
+    // </div>
+    <ListOfParkingDisplay />
   );
 }
 
 export default App;
+
+// export default ListOfParkingDisplay;
