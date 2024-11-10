56b13414ea0bf7787f0ebc374d4361d46aa37e70
njerem01 <njerem01@students.cs.ubc.ca>
2024-11-10 14:12:24 -0800

    begin

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div class="master">
+    <div>
       <h1>WELCOME TO</h1>
       <h1>PARKING LOT</h1>
       <div class="parent">
         <img src="./parking.img.png" alt="parking photo" class="image1" />
         <img src="./car.img.png" alt="car photo" class="image2" />
       </div>
-      <img src="./tBirdLeft.img.png" alt="bird photo" class="birdLeft" />
+      <div class="start">
+        <img src="./tBirdLeft.img.png" alt="bird photo" class="birdLeft" />
 
-      <button class="button">GET STARTED</button>
+        <a href="thumbs.js"><button class="button">GET STARTED</button></a>
 
-     <img src="./tBirdR.img.png" alt="bird photo" class="birdRight" />
+        <img src="./tBirdR.img.png" alt="bird photo" class="birdRight" />
+      </div>
     </div>
   );
 }
