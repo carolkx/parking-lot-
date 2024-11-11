332e8b5960eb526b55f4713fe9801a9d9e19663d
ftjcai10 <ftjcai10@students.cs.ubc.ca>
2024-11-10 16:01:14 -0800

    update thumb so it can sent a state to ListOfParking

--- src/thumb.js
+++ src/thumb.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./thumb.css";
 import thumbUpImage from "./thumbsUp.png";
 import thumbDownImage from "./thumbsDown.png";
 
 function Thumb() {
   const navigate = useNavigate();
+  const location = useLocation();
+  const{index} = location.state;
 
-  const handleThumbClick = () => {
-    navigate("/ListOfParking");
+  const handleThumbUpClick = () => {
+    navigate("/ListOfParking", {state: {index, changeType: "thumbUp"}});
+  };
+
+  const handleThumbDownClick = () => {
+    navigate("/ListOfParking",  {state: {index, changeType: "thumbDown"}});
   };
 
   return (
@@ -22,13 +28,13 @@ function Thumb() {
           src={thumbUpImage}
           alt="Thumb Up"
           className="thumb-button"
-          onClick={handleThumbClick}
+          onClick={handleThumbUpClick}
         />
         <img
           src={thumbDownImage}
           alt="Thumb Down"
           className="thumb-button"
-          onClick={handleThumbClick}
+          onClick={handleThumbDownClick}
         />
       </div>
     </div>
